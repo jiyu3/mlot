9f47e2a080a8521da8a881ea2ea98dcea5a7b27d
jiyu <jiyu@jiyus-MacBook-Pro.local>
2018-09-15 18:42:26 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">Home</router-link>
+			<router-link to="/edit">Edit</router-link>
+			<router-link to="/lot">Lot</router-link>
+			<router-link to="/lot">My</router-link>
+			<router-link to="/lot">Result</router-link>
+		</div>
+		<router-view/>
+	</div>
+</template>
+
+<style>
+body {
+	background: #e1fae1;
+}
+
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+#nav {
+	width: 100%;
+	position: fixed;
+	top: 10px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+#nav a:not(:last-child) {
+	margin-right: 10px;
+}
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			// lazy load
+			component: () => import(/* webpackChunkName: "about" */ './components/Home/index.vue')
+		},
+		{
+			path: '/edit',
+			name: 'Edit',
+			// lazy load
+			component: () => import(/* webpackChunkName: "about" */ './components/Edit/index.vue')
+		},
+		{
+			path: '/lot',
+			name: 'Lot',
+			// lazy load
+			component: () => import(/* webpackChunkName: "about" */ './components/Lot/index.vue')
+		},
+		{
+			path: '/my',
+			name: 'My',
+			// lazy load
+			component: () => import(/* webpackChunkName: "about" */ './components/My/index.vue')
+		},
+		{
+			path: '/result',
+			name: 'Result',
+			// lazy load
+			component: () => import(/* webpackChunkName: "about" */ './components/Result/index.vue')
+		}
+	]
+})
