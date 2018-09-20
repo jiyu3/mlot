23684a0ce65c85533be765f2a5f52200ea5673f7
jiyu <m@jiyu.lol>
2018-09-20 20:21:20 +0900

    email login

--- client/src/components/Login/index.vue
+++ client/src/components/Login/index.vue
@@ -17,7 +17,7 @@ export default {
 				return false
 			}
 
-			firebase.auth().sendSignInLinkToEmail(this.email, this.$store.getters.actionCodeSettings).then(function() {
+			firebase.auth().sendSignInLinkToEmail(this.email, this.$store.getters.actionCodeSettings).then(() => {
 				alert("email successfully sent!")
 				// The link was successfully sent. Inform the user.
 				// Save the email locally so you don't need to ask the user for it again
@@ -58,6 +58,37 @@ export default {
 			})
 			alert("ログインに失敗しました。もう一度やり直してください") // TODO 多言語化
 			self.$router.push("/")
+		}
+	},
+	mounted() {
+		// Confirm the link is a sign-in with email link.
+		if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+		// Additional state parameters can also be passed via URL.
+		// This can be used to continue the user's intended action before triggering
+		// the sign-in operation.
+		// Get the email if available. This should be available if the user completes
+		// the flow on the same device where they started it.
+		var email = window.localStorage.getItem('emailForSignIn');
+		if (!email) {
+			// User opened the link on a different device. To prevent session fixation
+			// attacks, ask the user to provide the associated email again. For example:
+			email = window.prompt('Please provide your email for confirmation');
+		}
+		// The client SDK will parse the code from the link for you.
+		firebase.auth().signInWithEmailLink(email, window.location.href)
+			.then(function(result) {
+			// Clear email from storage.
+			window.localStorage.removeItem('emailForSignIn');
+			// You can access the new user via result.user
+			// Additional user info profile not available via:
+			// result.additionalUserInfo.profile == null
+			// You can check if the user is new or existing:
+			// result.additionalUserInfo.isNewUser
+			})
+			.catch(function(error) {
+			// Some error occurred, you can inspect the code: error.code
+			// Common errors could be invalid email and invalid or expired OTPs.
+			});
 		}
 	}
 }
